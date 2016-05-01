9238f0c86720d9a6d898575864bd84df3b85eb51
rafa111989 <rafa111989@gmail.com>
2016-05-01 00:34:05 -0700

    adds basic functionality
    Uses city name in order to get the current temperature. Note that the
    output may not be exact for your current location.

--- Team_wEather/Team_wEather/Form1.Designer.cs
+++ Team_wEather/Team_wEather/Form1.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.btn_getWx = new System.Windows.Forms.Button();
             this.txtBx_Zip = new System.Windows.Forms.TextBox();
-            this.lbl_Zip = new System.Windows.Forms.Label();
-            this.lstBx_DisplayWx = new System.Windows.Forms.ListBox();
+            this.lbl_city = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblTemp_output = new System.Windows.Forms.Label();
+            this.lbl_tempFaren_Output = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_getWx
@@ -51,30 +53,49 @@
             this.txtBx_Zip.Size = new System.Drawing.Size(100, 20);
             this.txtBx_Zip.TabIndex = 1;
             // 
-            // lbl_Zip
+            // lbl_city
             // 
-            this.lbl_Zip.AutoSize = true;
-            this.lbl_Zip.Location = new System.Drawing.Point(12, 16);
-            this.lbl_Zip.Name = "lbl_Zip";
-            this.lbl_Zip.Size = new System.Drawing.Size(50, 13);
-            this.lbl_Zip.TabIndex = 2;
-            this.lbl_Zip.Text = "Zip Code";
+            this.lbl_city.AutoSize = true;
+            this.lbl_city.Location = new System.Drawing.Point(12, 16);
+            this.lbl_city.Name = "lbl_city";
+            this.lbl_city.Size = new System.Drawing.Size(55, 13);
+            this.lbl_city.TabIndex = 2;
+            this.lbl_city.Text = "City Name";
             // 
-            // lstBx_DisplayWx
+            // lblTemp
             // 
-            this.lstBx_DisplayWx.FormattingEnabled = true;
-            this.lstBx_DisplayWx.Location = new System.Drawing.Point(12, 39);
-            this.lstBx_DisplayWx.Name = "lstBx_DisplayWx";
-            this.lstBx_DisplayWx.Size = new System.Drawing.Size(273, 225);
-            this.lstBx_DisplayWx.TabIndex = 3;
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(15, 55);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(67, 13);
+            this.lblTemp.TabIndex = 3;
+            this.lblTemp.Text = "Temperature";
+            // 
+            // lblTemp_output
+            // 
+            this.lblTemp_output.AutoSize = true;
+            this.lblTemp_output.Location = new System.Drawing.Point(89, 55);
+            this.lblTemp_output.Name = "lblTemp_output";
+            this.lblTemp_output.Size = new System.Drawing.Size(0, 13);
+            this.lblTemp_output.TabIndex = 4;
+            // 
+            // lbl_tempFaren_Output
+            // 
+            this.lbl_tempFaren_Output.AutoSize = true;
+            this.lbl_tempFaren_Output.Location = new System.Drawing.Point(82, 72);
+            this.lbl_tempFaren_Output.Name = "lbl_tempFaren_Output";
+            this.lbl_tempFaren_Output.Size = new System.Drawing.Size(0, 13);
+            this.lbl_tempFaren_Output.TabIndex = 5;
             // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(307, 285);
-            this.Controls.Add(this.lstBx_DisplayWx);
-            this.Controls.Add(this.lbl_Zip);
+            this.Controls.Add(this.lbl_tempFaren_Output);
+            this.Controls.Add(this.lblTemp_output);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lbl_city);
             this.Controls.Add(this.txtBx_Zip);
             this.Controls.Add(this.btn_getWx);
             this.Name = "main";
@@ -88,8 +109,10 @@
 
         private System.Windows.Forms.Button btn_getWx;
         private System.Windows.Forms.TextBox txtBx_Zip;
-        private System.Windows.Forms.Label lbl_Zip;
-        private System.Windows.Forms.ListBox lstBx_DisplayWx;
+        private System.Windows.Forms.Label lbl_city;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblTemp_output;
+        private System.Windows.Forms.Label lbl_tempFaren_Output;
     }
 }
 
